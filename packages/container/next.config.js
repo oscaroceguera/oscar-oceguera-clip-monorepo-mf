6419e2a2133fcf781ab7991595c3c68d53508382
oscar <oscaroceguera@gmail.com>
2023-08-08 10:16:17 -0700

    Added nextjs-mf

--- packages/container/next.config.js
+++ packages/container/next.config.js
@@ -5,10 +5,13 @@ module.exports = {
     if (!options.isServer) {
       config.plugins.push(
         new NextFederationPlugin({
-          name: "home",
+          name: "container",
           filename: "static/chunks/remoteEntry.js",
           remotes: {
             marketing: `marketing@https://main.d1q1nicd5914fm.amplifyapp.com/remoteEntry.js`,
+          },
+          exposes: {
+            "./pages-map": "./pages-map.js",
           },
         })
       );
--- packages/container/pages/index.js
+++ packages/container/pages/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
@@ -9,7 +7,7 @@ export default function Home() {
   return (
     <>
       <h1 style={{ textAlign: "center", margin: "28px" }}>
-        Esta app va a transformarce en un contenedor MF Loco!
+        Esta app va a transformarce en un contenedor Microfront!
       </h1>
     </>
   );
